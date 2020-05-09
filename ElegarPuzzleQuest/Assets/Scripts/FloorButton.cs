533f6632bf4324eb3dd642e6daa91e4bd56527e4
fruitpunchsamurai <fruitpunchsamuraigr@gmail.com>
2020-05-09 04:49:28 -0700

    made a switch and a movable rock

---
+++ ElegarPuzzleQuest/Assets/Scripts/FloorButton.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorButton : MonoBehaviour
+{
+    [SerializeField]
+    Sprite pressedSprite;
+    [SerializeField]
+    Sprite notPressedSprite;
+    [SerializeField]
+    SpriteRenderer renderer;
+
+    bool isPressed = false;
+
+    private void Update()
+    {
+        if(isPressed)
+        {
+            renderer.sprite = pressedSprite;
+        }
+        else
+        {
+            renderer.sprite = notPressedSprite;
+        }
+    }
+
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        isPressed = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isPressed = false;
+    }
+
+}
