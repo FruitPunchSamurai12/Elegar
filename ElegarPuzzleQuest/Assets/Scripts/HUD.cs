7439800f102e755996e64a779eebe624764ca61e
fruitpunchsamurai <fruitpunchsamuraigr@gmail.com>
2020-05-20 16:22:13 -0700

    added hud

---
+++ ElegarPuzzleQuest/Assets/Scripts/HUD.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    [SerializeField]
+    Player elegar;
+
+    [SerializeField]
+    Sprite fullHeart;
+
+    [SerializeField]
+    Sprite halfHeart;
+
+    [SerializeField]
+    Sprite emptyHeart;
+
+
+    [SerializeField]
+    Image[] hearts;
+
+
+    public static HUD Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)//simpleton pattern
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int numberOfHearts = elegar.maxLife / 2;
+        for(int i =0; i<hearts.Length;i++)
+        {
+            if(i<numberOfHearts)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateHearts();
+    }
+
+    void UpdateHearts()
+    {
+        int currentHp = elegar.currentLife;
+        if (currentHp == 0)
+        {
+            for (int i = 0; i < hearts.Length; i++)
+            {
+                hearts[i].sprite = emptyHeart;
+            }
+        }
+        else
+        {
+            for (int i = 0; i < hearts.Length; i++)
+            {
+                if (currentHp % 2 == 0)
+                {
+                    if (i < currentHp / 2)
+                    {
+                        hearts[i].sprite = fullHeart;
+                    }
+                    else
+                    {
+                        hearts[i].sprite = emptyHeart;
+                    }
+                }
+                else
+                {
+                    if (i < currentHp / 2)
+                    {
+                        hearts[i].sprite = fullHeart;
+                    }
+                    else if (i == currentHp / 2)
+                    {
+                        hearts[i].sprite = halfHeart;
+                    }
+                    else
+                    {
+                        hearts[i].sprite = emptyHeart;
+                    }
+                }
+            }
+        }
+    }
+
+}
